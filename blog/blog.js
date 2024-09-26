62e0080440a86cd9bcb94bcccdd24f0bfc352160
Michael  Wheeler <mwheeler2244@yahoo.com>
2024-09-26 14:35:20 -0500

    added CSS blog

--- blog/blog.js
+++ blog/blog.js
@@ -1,15 +1,24 @@
 // social share
 
-
 // Facebook Share
-document.querySelector(".facebook-share").href = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(window.location.href)}`;
+document.querySelector(
+  ".facebook-share"
+).href = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
+  window.location.href
+)}`;
 
-// Instagram Share 
-document.querySelector(".instagram-share").href = `https://www.instagram.com`; 
+// Instagram Share
+document.querySelector(".instagram-share").href = `https://www.instagram.com`;
 // Twitter Share
-document.querySelector(".twitter-share").href = `https://twitter.com/share?url=${encodeURIComponent(window.location.href)}&text=Check%20out%20this%20Coding%20blog!`;
+document.querySelector(
+  ".twitter-share"
+).href = `https://twitter.com/share?url=${encodeURIComponent(
+  window.location.href
+)}&text=Check%20out%20this%20Coding%20blog!`;
 
 // LinkedIn Share
-document.querySelector(".linkedin-share").href = `https://www.linkedin.com/sharing/share-offsite/?url=${encodeURIComponent(window.location.href)}`;
-
-
+document.querySelector(
+  ".linkedin-share"
+).href = `https://www.linkedin.com/sharing/share-offsite/?url=${encodeURIComponent(
+  window.location.href
+)}`;
